6dcb59196f401e4bfa5be3b6c34c5ab245014292
Jacob Greenberg <jgreenberg45@yahoo.com>
2016-06-15 19:37:52 -0400

    Home page functional with images and CSS, started about page

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ server.get('/', function(request, response){
   response.sendFile('public/html/home.html', {root: __dirname});
 });
 
-//server.get('/about', function(request, response){
-  //response.sendFile('public/html/about.html', {root: __dirname});
-//});
+server.get('/about', function(request, response){
+  response.sendFile('public/html/about.html', {root: __dirname});
+});
 
 //server.get('/contact', function(request, response){
   //  response.sendFile('public/html/contact.html', {root: __dirname});
