9eef69d5d2ae4fe9ea387ea6ce16502cb57bcf40
Jacob Greenberg <jgreenberg45@yahoo.com>
2016-06-14 20:29:09 -0400

    Created resposity and got set up

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var server = express();
+
+server.get('/', function(request, response){
+  response.sendFile('public/html/index.html', {root: __dirname});
+});
+
+server.get('/about', function(request, response){
+  response.sendFile('public/html/about.html', {root: __dirname});
+});
+
+server.get('/contact', function(request, response){
+    response.sendFile('public/html/contact.html', {root: __dirname});
+});
+
+server.listen(8080, function(){
+  console.log('Now listening on 8080');
+});
